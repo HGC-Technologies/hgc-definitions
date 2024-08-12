7aeb7adc04833823641164aa2794fffcc3e93c4b
sheri-hgc <Sheridan.Perez.Antigua@wearehgc.com>
2024-08-12 14:28:24 -0400

    define the enum

---
+++ src/definitions.ts
@@ -0,0 +1,24 @@
+export const ScanDetailCategory = {
+    UX_UI :"UX/UI",
+    MANAGEABILITY:"Manageability",
+    UPGRADABILITY:"Upgradability",
+    SECURITY:"Security",
+    PERFORMANCE:"Performance"
+}
+
+export const ScanIssueType = {
+    SLOW_QUERIES :"Slow Queries",
+    LARGE_TABLES:"Large Tables",
+    LONG_TITLES:"Long Titles",
+    GLOBAL_UI_SCRIPTS:"Global UI Scripts",
+    ACL:"ACLs",
+    BUSY_FORMS:"Busy Forms",
+    SLOW_SCRIPTS:"Slow Scripts",
+    DATABASE_LOOKUPS_IN_ACCESS_CONTROL_RULES:"Database Lookups in Access Control Rules",
+    GLOBAL_BUSINESS_RULES:"Global Business Rules",
+    INSTANCE_ERROR_LOGS:"Instance Error Logs",
+    LONG_RUNNING_REPORTS:"Long Running Reports",
+    DUPLICATE_SCRIPT_INCLUDES:"Deplicate Script Includes",
+    DUPLICATE_FIELDS:"Duplicate Fields",
+    SCRIPT_INCLUDE_REPORTS:"Script Includes Reports",
+};
